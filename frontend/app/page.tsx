620b08639f0a01ab7de7331230ca4d51874ffe61
Alyan Shahid <alyanshahidsdk@gmail.com>
2026-02-15 16:51:38 +0500

    feat: Update the home page heading with new styling and refine Next.js build output ignores in `.gitignore`.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Home</h1>
+        <h1 className="text-5xl hover:text-6xl transition-all duration-500 first-letter:font-bold text-end">Welcome to <span className="text-amber-500">Origin</span></h1>
       </main>
     </div>
   );
